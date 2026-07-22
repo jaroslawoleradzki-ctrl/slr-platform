8c608a666a5f160adf212fafc371675d573a9281
jaroslawoleradzki-ctrl <jaroslaw.oleradzki@gmail.com>
2026-07-22 08:21:39 +0200

    feat(domain): export foundational domain models

---
+++ app/domain/__init__.py
@@ -0,0 +1,14 @@
+from app.domain.author import Affiliation, Author
+from app.domain.identifiers import Identifier, IdentifierType
+from app.domain.provenance import ProvenanceEntry
+from app.domain.venue import Venue, VenueType
+
+__all__ = [
+    "Affiliation",
+    "Author",
+    "Identifier",
+    "IdentifierType",
+    "ProvenanceEntry",
+    "Venue",
+    "VenueType",
+]
